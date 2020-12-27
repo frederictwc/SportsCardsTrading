c374bd80e92ce02bcfeed48b53579056793db5c4
frederictwc <frederictwc@live.com>
2020-12-27 22:07:47 +0800

    plot 2 cards same plot

--- plot.py
+++ plot.py
@@ -4,15 +4,22 @@ import matplotlib.dates as mdates
 import datetime as dt
 
 
-data = np.genfromtxt("data/A_Davis_PSA_10.txt",dtype=(str,str),delimiter=" ")
+data = np.genfromtxt("data/Zion_PSA_9.txt",dtype=(str,str),delimiter=" ")
 x = data[:,0]
 x = [dt.datetime.strptime(d,"%Y-%m-%d").date() for d in x]
 y = [float(element.replace(",","")) for element in data[:,1]]
+
+data_2 = np.genfromtxt("data/Zion_PSA_10.txt",dtype=(str,str),delimiter=" ")
+x_2 = data_2[:,0]
+x_2 = [dt.datetime.strptime(d,"%Y-%m-%d").date() for d in x_2]
+y_2 = [float(element.replace(",","")) for element in data_2[:,1]]
 
 #plot
 plt.title("Price vs date")
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
 plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=100))
 plt.plot(x,y)
+plt.plot(x_2,y_2)
+plt.legend(['Zion_PSA_9', 'Zion_PSA_10'])
 plt.gcf().autofmt_xdate()
 plt.show()
